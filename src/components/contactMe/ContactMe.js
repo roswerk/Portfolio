b3f61ec15efec0417344136360a2efd4f77e010f
Max <73542409+roswerk@users.noreply.github.com>
2021-09-21 17:57:39 +0200

    Improved UI of contact-me segment

--- src/components/contactMe/ContactMe.js
+++ src/components/contactMe/ContactMe.js
@@ -1,53 +1,57 @@
 import React from "react";
-import {Form, Button, InputGroup, FormControl} from "react-bootstrap"
-import "./contactMe.css"
+import { Form, Button, InputGroup, FormControl } from "react-bootstrap";
+import "./contactMe.css";
+import Card from "react-bootstrap/Card";
 
-function ContactMe(){
+function ContactMe() {
+  return (
+    <div clasName="formMain" id="contact">
+      <Card className="contactCard">
+        <div class="contactTitle">
+          <h2 className="contactTitle">Contact Me</h2>
+        </div>
 
-return(
-  <div clasName="formMain" id="contact">
-    <div class="contactTitle">
-    <h2 className="contactTitle">Contact Me</h2>
+        <Form className="formContent">
+          <div className="emailAndPass">
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicEmail"
+              className="emailAddress"
+            >
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="formBasicPassword"
+              className="password"
+            >
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Name" />
+            </Form.Group>
+          </div>
+
+          <div className="textbox">
+            <InputGroup>
+              <InputGroup.Text>Message</InputGroup.Text>
+              <FormControl
+                as="textarea"
+                aria-label="With textarea"
+                className="messageholder"
+              />
+            </InputGroup>
+
+            <div className="contactBtn">
+              <Button variant="primary" type="submit" className="contactButton">
+                Submit
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </Card>
     </div>
-    
-    <Form className="formContent">
-      
-      <div className="emailAndPass">
-
-  <Form.Group className="mb-3" controlId="formBasicEmail" className="emailAddress">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword" className="password">
-    <Form.Label>Name</Form.Label>
-    <Form.Control type="text" placeholder="Name" />
-  </Form.Group>
-
-  </div>
-  
-  <div className="textbox">
-
-  <InputGroup >
-    <InputGroup.Text >Menssage</InputGroup.Text>
-    <FormControl as="textarea" aria-label="With textarea" className="messageholder" />
-  </InputGroup>
-  
-
-<div className="contactBtn">
-  <Button variant="primary" type="submit" className="contactButton">
-    Submit
-  </Button>
-  </div>
-
-  </div>
-
-</Form>
-
-    
-  </div>
-)
-
+  );
 }
 
-export default ContactMe
+export default ContactMe;
