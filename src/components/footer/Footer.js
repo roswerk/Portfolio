e458377f64e54ea53b8ca50e3140622b89aa1627
Max <73542409+roswerk@users.noreply.github.com>
2021-09-21 18:01:10 +0200

    Updated LinkedIn link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,37 +1,45 @@
-import React from "react"
-import "./footer.css"
+import React from "react";
+import "./footer.css";
 
-function Footer(){
+function Footer() {
+  return (
+    <div>
+      <div className="footerSM">
+        <a
+          href="https://www.linkedin.com/in/maximilian-rosales-923565120/"
+          target="_blank"
+        >
+          <img
+            src="https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg"
+            alt="My LinkedIn"
+            height="34"
+            width="34"
+            className="footerSMIcons"
+          />
+        </a>
+        <a href="https://www.instagram.com/maxroswerk/" target="_blank">
+          <img
+            src="https://facebookbrand.com/wp-content/uploads/2021/03/Instagram_AppIcon_Aug2017.png?w=150&h=150"
+            alt="My Instagram"
+            height="34"
+            width="34"
+            className="footerSMIcons"
+          />
+        </a>
+        <a href="https://twitter.com/MaximilianRosa2" target="_blank">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/300px-Twitter_bird_logo_2012.svg.png"
+            alt="My Twitter"
+            height="34"
+            width="40"
+            className="footerSMIcons"
+          />
+        </a>
+      </div>
 
-return(
-  <div>
-
-  <div className="footerSM">
-  <a href="https://www.linkedin.com/in/max-rosales-923565120/"
-    target="_blank">
-  <img src="https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg" 
-  alt="My LinkedIn" height="34" width="34" 
-  className="footerSMIcons"/>
-  </a>
-  <a href="https://www.instagram.com/maxroswerk/"
-    target="_blank">
-  <img src="https://facebookbrand.com/wp-content/uploads/2021/03/Instagram_AppIcon_Aug2017.png?w=150&h=150" 
-  alt="My Instagram" height="34" width="34" 
-  className="footerSMIcons"/>
-  </a>
-  <a href="https://twitter.com/MaximilianRosa2"
-    target="_blank">
-  <img src="https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/300px-Twitter_bird_logo_2012.svg.png" 
-  alt="My Twitter" height="34" width="40" 
-  className="footerSMIcons"/>
-  </a>
-  </div>
-
-  <div className="footer">Roswerk 2021</div>
-
-  </div>
-)
-
+      <div className="footer">Roswerk 2021</div>
+    </div>
+  );
 }
 
 export default Footer;
