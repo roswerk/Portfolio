0ef87be0eee900264b25917c81f37b1afe71e668
Max <73542409+roswerk@users.noreply.github.com>
2021-09-21 17:57:25 +0200

    Included SM icons in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -16,7 +16,13 @@ return(
   <a href="https://www.instagram.com/maxroswerk/"
     target="_blank">
   <img src="https://facebookbrand.com/wp-content/uploads/2021/03/Instagram_AppIcon_Aug2017.png?w=150&h=150" 
-  alt="My LinkedIn" height="34" width="34" 
+  alt="My Instagram" height="34" width="34" 
+  className="footerSMIcons"/>
+  </a>
+  <a href="https://twitter.com/MaximilianRosa2"
+    target="_blank">
+  <img src="https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/300px-Twitter_bird_logo_2012.svg.png" 
+  alt="My Twitter" height="34" width="40" 
   className="footerSMIcons"/>
   </a>
   </div>
