55ca142b35cb446f6dbf2313e07cbf24e98d3d97
Max <73542409+roswerk@users.noreply.github.com>
2021-09-24 18:40:41 +0200

    Refactored about section to make it responsive

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,31 +1,53 @@
-import React from "react"
-import "./about.css"
-import {Button} from "react-bootstrap"
+import React from "react";
+import "./about.css";
+import { Button } from "react-bootstrap";
+import CV from "../../resources/Rosales-Maximilian-Resume.pdf";
 
-function About(){
+function About() {
+  return (
+    <div className="mainContainer" id="about">
+      <div className="background">
+        <div className="photoDiv">
+          <img
+            src="https://ca.slack-edge.com/T03DJ0G01-U01NEKBSPGX-3789eeff614a-512"
+            alt="Maximilians Photo"
+            className="photo"
+          ></img>
+        </div>
 
-return(
-<div className="mainContainer" id="about">
-<div className="background">
-<div className="photoDiv">
-<img src="https://ca.slack-edge.com/T03DJ0G01-U01NEKBSPGX-3789eeff614a-512" alt="Maximilians Photo" className="photo"></img>
-</div>
+        <div className="info">
+          <h2>Hello, my name is Maximilian Rosales</h2>
+          <p>I'm a Full-Stack Web Developer</p>
 
-<div className="info">
-  <h2>Hello, my name is Maximilian Rosales</h2>
-  <p>I'm a full stack Web Developer</p>
-
-  <div>
-  <Button variant="outline-light" className="infoButton" href="https://github.com/roswerk" target="_blank">Go to GitHub</Button>
-  <Button variant="outline-light" className="infoButton" href="#projects">Go to Projects</Button>
-  </div>
-
-</div>
-</div>
-
-  </div>
-)
-
+          <div className="profileButtons">
+            <Button
+              variant="outline-light"
+              className="infoButton"
+              href="https://github.com/roswerk"
+              target="_blank"
+            >
+              Go to GitHub
+            </Button>
+            <Button
+              variant="outline-light"
+              className="infoButton"
+              href="#projects"
+            >
+              Go to Projects
+            </Button>
+            <Button
+              variant="outline-light"
+              className="infoButton"
+              href={CV}
+              target="_blank"
+            >
+              Go to CV
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
